79bc5c067c385795c13279642e9819b9adfa62cc
Fred Sauer <fredsa@google.com>
2013-10-31 11:39:34 -0700

    udpated status message when inserting a guest
    Reviewed in https://codereview.appspot.com/20170045/

--- app/js/app.js
+++ app/js/app.js
@@ -85,7 +85,7 @@ App.controller('InsertCtrl', function($scope, $rootScope, $log, $http, $routePar
       first : $scope.first,
       last : $scope.last, 
     };
-    $rootScope.status = 'Saving...';
+    $rootScope.status = 'Creating...';
     $http.post('/rest/insert', guest)
     .success(function(data, status, headers, config) {
       $rootScope.guests.push(data);
